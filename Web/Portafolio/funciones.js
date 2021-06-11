b0541aa6c8b6c92cea942d2a5c76d3b298a1dc58
BrayanCahuana <cahuanabrayan123@gmail.com>
2021-06-11 00:47:04 -0500

    avance mi portafolio, agregue menu responsive

---
+++ Web/Portafolio/funciones.js
@@ -0,0 +1,24 @@
+
+//codigo para el menu hamburguesa
+    function menuHamburguesa (){
+        let menu = document.getElementById('boton-menu');
+        let direcciones = document.getElementById('direcciones')
+        let barNav = document.getElementById('nav')
+        if (menu.checked == true){
+            direcciones.style.transition = '1s'
+            direcciones.style.transform = 'translateX(100%)'
+            barNav.style.backgroundColor = 'rgba(0, 0, 0, 0.137)'
+
+        }
+        else{
+            direcciones.style.transition = '1s'
+            direcciones.style.transform = 'translateX(0%)'
+            barNav.style.transition = '1s'
+            barNav.style.backgroundColor = 'rgba(28, 36, 36, 0.972)'
+        }
+    
+    }
+//codigo para el envio del formulario
+    function enviarMensaje () {
+        alert("Los mensajes no se podran enviar por el momento, no cuento con servidor habilitado. Contactame por redes mis redes sociales!!")
+    }
