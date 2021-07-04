4ed2bab6efa90b4fc5c907618994bb0068092699
BrayanCahuana <cahuanabrayan123@gmail.com>
2021-07-04 01:56:04 -0500

    se modifico los mensajes

--- Web/Local de eventos/funciones.js
+++ Web/Local de eventos/funciones.js
@@ -3,8 +3,18 @@
 window.onload = function() {
     spinner = document.getElementById('preloader')
     spinner.style.opacity = '0'
+    spinner.style.zIndex = '-1'
 }
 
+// Mostrar elementos cuando haces scroll
+window.addEventListener('scroll',function(){
+    prueba = document.getElementById('servicios-pg-principal')
+    posicion = window.scrollY
+    // console.log(posicion)
+    if (posicion >= 1900){
+        prueba.style.opacity = '100%'
+    }
+})
 
 // navbar efecto scroll
 let PosicionPrincipal = window.pageYOffset
@@ -39,16 +49,23 @@ function menuHamburguesa () {
     let navenlaces = document.getElementById('enlaces')
     let navlogo = document.getElementById('logo')
     let icono = document.getElementById('menu-icono')
+    let redesw = document.getElementById('redesw')
+    let redesf = document.getElementById('redesf')
+
     if (navmenu.checked == true) {
         navenlaces.style.transform = 'translateY(0%)'
         navenlaces.style.backgroundColor = 'white'
         navenlaces.style.opacity = '100%'
         navlogo.style.filter = 'invert(100%)'
         icono.style.filter = 'invert(0%)'
+        redesw.style.filter = 'invert(100%)'
+        redesf.style.filter = 'invert(100%)'
     }
     else{
         navenlaces.style.transform = 'translateY(-100%)'
         navlogo.style.filter ='invert(0%)'
         icono.style.filter = 'invert(100%)'
+        redesw.style.filter = 'invert(0%)'
+        redesf.style.filter = 'invert(0%)'
     }
 }
